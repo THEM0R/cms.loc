779b314acb371dd0f80992ef17e014f010f3822c
THEM0R <the.mor.ua@gmail.com>
2023-03-18 19:02:58 +0200

    DI 0.0.3

---
+++ engine/DI/DI.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Engine\DI;
+
+class DI
+{
+  /**
+   * @var array
+   * Контейнер залежностей
+   */
+  private $container = [];
+
+
+  /**
+   * @param $key
+   * @param $value
+   * @return $this
+   */
+  public function set($key, $value)
+  {
+    $this->container[$key] = $value;
+
+    return $this;
+  }
+
+  /**
+   * @param $key
+   * @return mixed
+   */
+  public function get($key)
+  {
+    return $this->has($key);
+  }
+
+  /**
+   * @param $key
+   * @return bool
+   */
+  public function has($key){
+    return isset($this->container[$key]);
+  }
+
+}
