3ff8c219868c9e76263c34ab5343a2ad78040a53
THEM0R <the.mor.ua@gmail.com>
2023-03-19 15:32:51 +0200

    first Service 0.1.5

--- engine/Service/Database/Provider.php
+++ engine/Service/Database/Provider.php
@@ -3,15 +3,24 @@
 namespace Engine\Service\Database;
 
 use Engine\Service\AbstractProvider;
+use Engine\Core\Database\Connection;
 
 class Provider extends AbstractProvider
 {
+
+  /**
+   * @var string
+   */
+  public $serviceName = 'db';
+
   /**
    * @return mixed|void
    */
-  function init()
+  public function init()
   {
     // TODO: Implement init() method.
+    $db = new Connection();
 
+    $this->di->set($this->serviceName, $db);
   }
 }
